ea8578865380090a9bbb931d87fa51cabac3075b
Rodrigo Sztelzer <sztelzer@gmail.com>
2015-12-15 19:31:12 -0200

    feedbacks submit

--- feedbacks.js
+++ feedbacks.js
@@ -48,17 +48,61 @@
 		return directive
 	}
 
-	function feedbacksController(feedbacks){
+	function feedbacksController(feedbacks, auth, $http, config, $q){
 		var vm = this;
 		vm.list = feedbacks.list;
+		vm.toSend = 0;
+		vm.allSent = false;
+		vm.listSize = 0;
+
 		var get = feedbacks.get();
 		get.then(
 			function(resolve) {
-				vm.list = feedbacks.list
+				vm.list = feedbacks.list;
+				vm.listSize = vm.list.length;
 			},
 			function(reject) {
 			}
 		);
+
+		function put(feedback){
+			var req_config = {headers: {'Authorization': auth.user.token}};
+			var path = feedback.path;
+			var payload = {float: feedback.object.float, open: "false"};
+
+			return $q(function(resolve, reject) {
+				$http.patch(config.api + path, payload, req_config)
+					.then(
+					function successCallback(response) {
+						resolve(response.data);
+					},
+					function errorCallback(response) {
+						reject();
+					});
+			});
+		}
+
+		vm.send = send;
+		function send(){
+			Object.keys(vm.list).forEach(function(key){
+				var feedback = vm.list[key];
+				if (feedback.object.float != 0) {
+					vm.toSend -= 1;
+					delete vm.list[key];
+					if (vm.toSend == 0){
+						vm.allSent = true;
+					}
+					put(feedback).then(
+						function(resolve){
+							vm.list[key].object = resolve.object;
+						},
+						function(reject){
+							vm.list[key].object.open = "false";
+						}
+					);
+				} //end if
+			});
+		}
 
 
 
